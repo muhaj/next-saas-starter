856393a3404a9cf1f6a5a8c613e83d3dab2c79ca
Jetnor Muhaj <jmuhaj@gmail.com>
2023-04-29 18:25:29 +0100

    Update Hero.tsx

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,8 +14,8 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>the coolest, ai no-code automation you have ever seen</CustomOverTitle>
+        <Heading>Make your life easier with our Genie</Heading>
         <Description>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
           repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
